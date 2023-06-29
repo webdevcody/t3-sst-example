0662f1255041b5b383376c56bd86a1f54a761e18
Web Dev Cody <webdevcody@gmail.com>
2023-06-28 20:53:24 -0400

    setting up deployment

---
+++ sst.config.ts
@@ -0,0 +1,24 @@
+import { SSTConfig } from "sst";
+import { NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "t3-sst",
+      region: "us-east-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+          DATABASE_URL: process.env.DATABASE_URL,
+        },
+      });
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
